5ec317d8d3e483cbd6c2bdb834432d9f29582aa4
Waza Kaunda <wkaunda@resolvesp.com>
2017-04-19 01:32:16 +0200

    Adding application java source

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,15 @@
+package clinic.programming.training;
+
+public class Application{
+	
+	public Application(){
+		System.out.println("Inside Application");
+	}
+	
+	// method main(): Always the application entry point
+	public static void main(String[] args){
+		
+		System.out.println("Starting Application");
+		Application app = new Application();
+	}
+}
